9581e7ed9f7da58c20a871690c745d7f87c91e4c
Alex <lexamart1708@gmail.com>
2021-03-07 11:43:26 +0300

    feat: add pagination and select examples

--- client/src/pages/Styles/Styles.js
+++ client/src/pages/Styles/Styles.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { state, useState } from 'react';
 
-import 'materialize-css';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 export const StylesPage = () => {
-  return (      <nav>
+  return (
+    <>
+      <nav>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">Logo</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
@@ -12,6 +15,24 @@ export const StylesPage = () => {
             <li><a href="collapsible.html">JavaScript</a></li>
           </ul>
         </div>
+
       </nav>
+      <ul class="pagination">
+        <li class="disabled"><a href="#!"><i class="material-icons"><ChevronLeftIcon /></i></a></li>
+        <li class="active"><a href="#!">1</a></li>
+        <li class="waves-effect"><a href="#!">2</a></li>
+        <li class="waves-effect"><a href="#!">3</a></li>
+        <li class="waves-effect"><a href="#!">4</a></li>
+        <li class="waves-effect"><a href="#!">5</a></li>
+        <li class="waves-effect"><a href="#!"><i class="material-icons"><ChevronRightIcon /></i></a></li>
+      </ul>
+      <label>Browser Select</label>
+      <select class="browser-default">
+        <option value="" disabled selected>Choose your option</option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+    </>
   )
 }
