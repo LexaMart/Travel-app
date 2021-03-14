b211de5207d111f5a4ea04fb1b87469d6d68a33d
Alex <lexamart1708@gmail.com>
2021-03-14 19:09:58 +0300

    fix: multer path of image

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage(
   {
     destination: './users-avatars/',
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + Date.now() + '-' + Math.round(Math.random() * 1E9 + ".png"));
+      cb(null, file.fieldname + Date.now() + '-' + Math.round(Math.random() * 1E9));
     }
   }
 );
