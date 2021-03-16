5b5c8fc06472586b7285f7236b398bbd7c6a2045
Nerbet <gdruzhina@mail.ru>
2021-03-17 02:06:36 +0300

    fix: fixed footer

--- client/src/pages/components/footer/footer.js
+++ client/src/pages/components/footer/footer.js
@@ -4,14 +4,15 @@ import './footer.css';
 
 export const Footer = () => {
   return (
-    <footer class="page-footer" id="footer_id">
-      <div class="container footer_inf">
-        <a href="https://rs.school/js/"><img class='course_logo' alt="rsschool" src="https://rs.school/images/rs_school_js.svg"></img></a>
-        <div class="white-text year">2021</div>
-        <div class="links">
-          <a class="white-text us" href="https://github.com/Nerbet">Nerbet</a>
-          <a class="white-text us" href="https://github.com/LexaMart">LexaMart</a>
+    <footer className="page-footer" id="footer_id">
+      <div className="container footer_inf">
+        <a href="https://rs.school/js/"><img className='course_logo' alt="rsschool" src="https://rs.school/images/rs_school_js.svg"></img></a>
+        <div className="white-text year">2021</div>
+        <div className="links">
+          <a className="white-text us" href="https://github.com/Nerbet">Nerbet</a>
+          <a className="white-text us" href="https://github.com/LexaMart">LexaMart</a>
         </div>
+        <a className="red-text youtube" href="">Youtube</a>
       </div>
     </footer>
   )
